d7e15abf7f756d03f30930cc6d8b95828937fec4
Artemoire <dejan.tot@gmail.com>
2023-09-11 01:44:33 +0200

    refactor: better line tracking

--- graphics/render/AppRenderer.ts
+++ graphics/render/AppRenderer.ts
@@ -6,12 +6,13 @@ const GOTO_LINE_START = '\r';
 const CLEAR_SCREEN = '\x1b[2J';
 
 type LineHistory = {
-  positions: [number,number][]
+  positions: [number, number][]
 }
 class AppRenderer implements ElementRenderer {
 
   private history = {
-    lineCount: 0
+    lineCount: 0,
+    caret: 0,
     // lines: [] as LineHistory[],
     // stdout: {
     //   cols: process.stdout.columns,
@@ -25,15 +26,26 @@ class AppRenderer implements ElementRenderer {
   render() {
     process.stdout.write(GOTO_LINE_START);
     process.stdout.write(GOTO_LINE_ABOVE.repeat(this.history.lineCount));
+    const maxCaret = process.stdout.columns;
     this.history.lineCount = 0;
+    this.history.caret = 0;
     const renderStrings = this.element.render(this);
     for (let i = 0; i < renderStrings.length; i++) {
+      if (this.history.caret === maxCaret) {
+        this.history.caret = 0;
+        this.history.lineCount++;
+      }
+
       const renderString = renderStrings[i];
 
-      // const isEscapeSequence = renderString['0'] === '\x1b';
-      if (renderString === '\n') this.history.lineCount++;
+      const isEscapeSequence = renderString['0'] === '\x1b';
 
       process.stdout.write(renderString);
+      if (!isEscapeSequence) this.history.caret += renderString.length;
+      if (renderString === '\n') {
+        this.history.caret = 0;
+        this.history.lineCount++;
+      }
 
     }
   }
